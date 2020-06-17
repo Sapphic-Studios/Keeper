0634c42f7f030cee4ee8f761e362449c18a810ef
tilond2 <tilond2@gmail.com>
2020-06-17 12:45:38 -0400

    Added platform test scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float timer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer -= 0.01f;
+        if (Input.GetMouseButtonDown(0))
+        {
+            timer = 0.2f;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        GameObject wall = collision.gameObject;
+        rb.velocity = new Vector2(0f, 0f);
+        Vector3 euler = wall.transform.rotation.eulerAngles;
+        Quaternion rot = Quaternion.Euler(0, 0, euler.z);
+        transform.rotation = rot;
+        print("name is " + wall + "and rotation is " + euler.z);
+        print("hit");
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (timer < 0) rb.velocity = new Vector2(0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PointAndMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndMove : MonoBehaviour
+{
+    private Vector3 target;
+    public GameObject pointer;
+    public GameObject player;
+    public GameObject arrow;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        pointer.transform.position = new Vector2(target.x, target.y);
+
+        Vector3 difference = target - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            moveInDirection(direction, rotationZ);
+        }
+    }
+    void moveInDirection(Vector2 direction, float rotationZ)
+    {
+        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        player.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}

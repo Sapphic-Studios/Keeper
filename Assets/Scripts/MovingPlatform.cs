ff9f04d29a87fb000d6c1eb5b9d172bc9ef9835c
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2020-06-18 15:57:16 -0400

    hazards and moving platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [SerializeField] private float SpeedX;
+    [SerializeField] private float SpeedY;
+    private Vector3 starting_position;
+    //public bool going_right;
+    //public bool going_up;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      starting_position = transform.position;
+      Debug.Log("starting postion: " + starting_position);
+      Debug.Log("x: " + starting_position.x);
+      Debug.Log("y: " + starting_position.y);
+      Debug.Log("z: " + starting_position.z);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      transform.position = new Vector3( (transform.position.x + (SpeedX * Time.deltaTime)), (transform.position.y + (SpeedY * Time.deltaTime)), transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+      if (collision.gameObject.tag == "Player"){
+        //moving = true;
+        collision.collider.transform.SetParent(transform);
+      }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+      if (collision.gameObject.tag == "Player"){
+        //moving = true;
+        collision.collider.transform.SetParent(null);
+      }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float timer; 
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +18,19 @@ public class Player : MonoBehaviour
     {
         timer -= 0.01f;
         if (Input.GetMouseButtonDown(0))
-        {
+        { 
+            Debug.Log("Pressed left click.");
             timer = 0.2f;
         }
-    }
+        if(Input.GetMouseButtonDown(0))
+
+        if(Input.GetMouseButtonDown(1))
+          Debug.Log("Pressed right click.");
+        if(Input.GetMouseButtonDown(2))
+          Debug.Log("Pressed middle click.");
+
+      }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
 
@@ -30,8 +39,8 @@ public class Player : MonoBehaviour
         Vector3 euler = wall.transform.rotation.eulerAngles;
         Quaternion rot = Quaternion.Euler(0, 0, euler.z);
         transform.rotation = rot;
-        print("name is " + wall + "and rotation is " + euler.z);
-        print("hit");
+        Debug.Log("name is " + wall + "and rotation is " + euler.z);
+        Debug.Log("hit");
     }
     void OnCollisionStay2D(Collision2D collision)
     {

03baf98de2b6c5ccfc0583bd02bed717b0b268d8
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2020-06-22 17:18:53 -0400

    some changes

--- Assets/Scripts/PlatformLerper.cs
+++ Assets/Scripts/PlatformLerper.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlatformLerper : MonoBehaviour
 {
-  Tilemap thisSprite;
+  Tilemap thisTilemap;
   [SerializeField] [Range(0f, 1f)] float lerpTime = 0;
   [SerializeField] Color[] myColors;
   int colorIndex = 0;
@@ -14,14 +15,29 @@ public class PlatformLerper : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    thisSprite = GetComponent<Tilemap>();
+    thisTilemap = GetComponent<Tilemap>();
     len = myColors.Length;
   }
 
   // Update is called once per frame
   void Update()
   {
-    thisSprite.color = Color.Lerp(thisSprite.color, myColors[colorIndex], lerpTime*Time.deltaTime);
+
+    BoundsInt bounds = thisTilemap.cellBounds;
+    TileBase[] allTiles = thisTilemap.GetTilesBlock(bounds);
+    for (int x = 0; x < bounds.size.x; x++) {
+        for (int y = 0; y < bounds.size.y; y++) {
+            TileBase tile = allTiles[x + y * bounds.size.x];
+            if (tile != null) {
+                Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+            } else {
+                Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+            }
+        }
+    }
+    
+    //thisTilemap.setColor = Color.Lerp(thisTilemap.color, myColors[colorIndex], lerpTime*Time.deltaTime);
+
     t = Mathf.Lerp(t, 1f, lerpTime*Time.deltaTime);
     if(t > .9f){
       t = 0f;

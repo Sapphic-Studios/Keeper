da6da21bd8558b8555e0fc943b49ccd3ceae9e03
tilond2 <tilond2@gmail.com>
2020-06-23 16:38:53 -0400

    Added Platform Script

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    GameObject player;
+    Player script;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        script = player.GetComponent<Player>();
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit");
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            rb.velocity = new Vector2(0f, 0f);
+            rb.gravityScale = 10;
+            
+            Vector3 euler = transform.rotation.eulerAngles;
+            Quaternion rot = Quaternion.Euler(0, 0, euler.z);
+            player.transform.rotation = rot;
+            int angle = (int)euler.z;
+            switch (angle)
+            {
+                case 0: //Ground
+                    script.grav = new Vector2(0, -1);
+                    Debug.Log("On Ground");
+                    break;
+                case 90: //Left facing wall
+                    script.grav = new Vector2(1, 0);
+                    Debug.Log("On Left Wall");
+                    break;
+                case 180: //Ceiling
+                    script.grav = new Vector2(0, 1);
+                    Debug.Log("On Ceiling");
+                    break;
+                case 270: //Right facing wall
+                    script.grav = new Vector2(-1, 0);
+                    Debug.Log("On Right Wall");
+                    break;
+            }
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("on");
+        if (script.timer>0)rb.velocity = new Vector2(0f, 0f);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            int rot = (int)transform.rotation.z;
+            script.grav = new Vector2(0, 0);
+            rb.gravityScale = 10;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    public Vector2 grav;
     public float timer;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,11 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         timer -= 0.01f;
+        Physics2D.gravity = grav;
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Pressed left click.");
+            
             timer = 0.2f;
         }
         if(Input.GetMouseButtonDown(0))
@@ -28,24 +31,17 @@ public class Player : MonoBehaviour
           Debug.Log("Pressed right click.");
         if(Input.GetMouseButtonDown(2))
           Debug.Log("Pressed middle click.");
-
       }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
         GameObject wall = collision.gameObject;
-        rb.velocity = new Vector2(0f, 0f);
-        Vector3 euler = wall.transform.rotation.eulerAngles;
-        Quaternion rot = Quaternion.Euler(0, 0, euler.z);
-        transform.rotation = rot;
-        Debug.Log("name is " + wall + "and rotation is " + euler.z);
-        Debug.Log("hit");
+        
+        //grav = new Vector2(0, 0);
+        //Debug.Log("name is " + wall + "and rotation is " + euler.z);
+        
     }
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        if (timer < 0) rb.velocity = new Vector2(0f, 0f);
-    }
+    
     void OnCollisionExit2D(Collision2D collision)
     {
         

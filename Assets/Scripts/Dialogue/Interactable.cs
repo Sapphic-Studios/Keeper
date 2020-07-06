cf76f4be9223a638520d4062f6013e2ba04d00bb
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2020-07-06 16:44:45 -0400

    fixed dialogue

--- Assets/Scripts/Dialogue/Interactable.cs
+++ Assets/Scripts/Dialogue/Interactable.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
 
-    [SerializeField]
-    
+    [SerializeField] public DialogueTrigger DT;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+      Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+      if (collision.gameObject.tag == "Player"){
+        DT.TriggerDialogue();
+      }
+
+    }
 
 }

22ea97a3fc60db07ad18cc08bfedf97007df066c
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2020-07-17 13:07:23 -0400

    somethin

---
+++ Assets/Scripts/PuzzleScripts/Puzzle2/DoorP2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorP2 : MonoBehaviour
+{
+    [SerializeField]
+    public string[] PATTERN;
+    [SerializeField]
+    public string objectname;
+//--------------------------------
+    private string[] PLAYERINPUT;
+    private int index = 0;
+    private bool isCorrect = true;
+    private bool unlocked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (!unlocked){
+        isCorrect = true;
+        /*for(int i=0; i < BUTTONS.Length; i++){
+          Debug.Log(BUTTONS[i].on + " != " +PATTERN[i]);
+          if(BUTTONS[i].on != PATTERN[i]){
+
+            isCorrect = false;
+          }*/
+        }
+
+        if(isCorrect){
+          GameObject.Find(objectname).SetActive(false);
+          unlocked=true;
+        }
+
+    }
+
+    public void AddtoArray(string platColor){
+      if(PLAYERINPUT[index] != platColor){
+          PLAYERINPUT[index] != platColor;
+          index +=1;
+          if(index == PLAYERINPUT.Length{
+            index =0;
+          }
+      }
+
+
+    }
+
+}
---
+++ Assets/Scripts/PuzzleScripts/Puzzle2/PlatformP2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformP2 : MonoBehaviour
+{
+
+    [SerializeField]
+    public string platformColor;
+//-------------------
+    private GameObject GameManager;
+    private DoorP2 Door; //unfortunately non-descriptive name
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      GameManager = GameObject.Find("GameManager");
+      Door = GameManager.GetComponent<DoorP2>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision){
+      if (collision.gameObject.tag == "Player"){
+        Door.AddtoArray(platformColor);
+      }
+    }
+
+    private void addtoDoorArray(){
+      Door.AddtoArray(platformColor);
+    }
+
+}

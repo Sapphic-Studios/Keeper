d1ed4703ca762e2a28a3aa00bd825ecb8cd4c7cd
tilond2 <tilond2@gmail.com>
2020-07-23 04:27:44 -0400

    Camera edge scrolling

--- Assets/Scripts/SysManagement/CameraController.cs
+++ Assets/Scripts/SysManagement/CameraController.cs
@@ -11,22 +11,54 @@ public class CameraController : MonoBehaviour
     public float minY, maxY;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    private Vector3 mousePos;
+    public Vector3 mousePos;
+    public float dist;
+    public float value;
+    float edgeSize = 3f;
+    public float width = Screen.width;
+    public float mouseX = Input.mousePosition.x;
+    public float offsetX = 8f;
+    public float offsetY = 4.7f;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-    }
+        offsetX = 8f;
+        offsetY = 4.7f;
+}
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        mouseX = Input.mousePosition.x;
         Vector3 targetPos = player.position;
         mousePos = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -100f));
         targetPos = new Vector3(Mathf.Clamp(player.position.x, minX, maxX), Mathf.Clamp(player.position.y, minY, maxY), -100f);
 
-        Vector3 difference = mousePos - targetPos;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos+ difference*(0.1f), ref velocity, smoothSpeed);
+        Vector3 difference = new Vector3 (mousePos.x - targetPos.x, mousePos.y - targetPos.y,0);
+        dist = difference.magnitude;
+        value = ((Mathf.Log(dist)) * 0.1f);
+        /*
+        if (difference.magnitude > 5f) dist = difference.magnitude;
+        else dist = 1000f;*/
+        if (Input.GetKey("d") || mousePos.x > player.position.x + offsetX)
+        {
+            targetPos.x += 100f * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || mousePos.x < player.position.x - offsetX)
+        {
+            targetPos.x -= 100f * Time.deltaTime;
+        }
+        if (Input.GetKey("w") || mousePos.y > player.position.y + offsetY)
+        {
+            targetPos.y += 100f * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || mousePos.y < player.position.y - offsetY)
+        {
+            targetPos.y -= 100f * Time.deltaTime;
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothSpeed);
+        
         
     }
 }
